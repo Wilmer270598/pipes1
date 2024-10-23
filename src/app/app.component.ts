5dba85b84d4f81c22524c48040f534c88ec7ac6b
wilmer <origami.18.wmca@gmail.com>
2024-10-22 19:00:30 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MiAppPipe';
+  title = '07-Pipes';
+
+  // Propiedades de tipo string
+  nombre: string = 'MiNombre Ejemplo';
+  nombre2: string = 'MiNombre2 Ejemplo';
+  lenguajes: string[] = ['C#', 'Java', 'Python', 'TypeScript', 'JavaScript'];
+
+  // Propiedades numéricas
+  PI: number = Math.PI;
+  porcentaje: number = 0.235;
+  salario: number = 1234.5;
+
+  // Propiedades de otros tipos
+  fecha: Date = new Date();
+  activar: boolean = true;
+  idioma: string = 'en';
+  videourl: string = 'https://www.youtube.com/embed/qZMBUPT3us';
+
+  // Promesa para simular una carga de datos asíncrona
+  valorPromesa: Promise<string> = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  // Objeto con propiedades anidadas
+  persona = {
+    nombre: 'MiNombre',
+    Paterno: 'Apellido',
+    edad: 0,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DomseguroPipe,
+    ContrasenaPipe,
+    CapitalizadoPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule,
